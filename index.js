b5d8b9e9a7ae90710103c2dbc20e507a92873f3e
Tim de Koning <git@king-management.nl>
2017-03-23 10:03:32 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"extends": "airbnb",
+	"rules": {
+		"react/jsx-filename-extension": [0],
+		"react/prefer-stateless-function": [0],
+		"react/react-in-jsx-scope": [0],
+		"global-require": [0],
+		"comma-dangle": [0],
+		"no-param-reassign": [0]
+	}
+};
