15c65aa59aab0f02684ee39ebb918e7950d96cb8
Kingsquare BV <source@kingsquare.nl>
2017-04-06 10:06:18 +0200

    no default case

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ module.exports = {
 		"comma-dangle": [0],
 		"no-param-reassign": [0], // e.g. reduce array to object
 		"no-underscore-dangle": [0],
+		"default-case": [0], // no default case in a switch is fine...
 		"max-len": ["error", 120, {
 			"ignoreTemplateLiterals": true,
 			"ignorePattern": "\\s*<" //jsx hack https://twitter.com/timtyrrell/status/689912501165658112
