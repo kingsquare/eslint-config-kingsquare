8f7528dfc636a3a90112aefd8e2278815b814c15
Tim de Koning <git@king-management.nl>
2018-11-09 09:44:35 +0100

    New standards:
    - Default eslint standards +
    - Default prettier standards

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
 module.exports = {
-	"parser": "babel-eslint", // https://github.com/eslint/eslint/issues/4683#issuecomment-284146481
-	"extends": "airbnb",
-	"rules": {
-		"react/jsx-filename-extension": [0],
-		"react/prefer-stateless-function": [0],
-		"react/react-in-jsx-scope": [0],
-		"global-require": [1],
-		"comma-dangle": [0],
-		"no-param-reassign": [0], // e.g. reduce array to object
-		"no-underscore-dangle": [0],
-		"default-case": [0], // no default case in a switch is fine...,
-        "function-paren-newline": [2, "consistent"], // fix for https://github.com/airbnb/javascript/issues/1584
-		"max-len": ["error", 120, {
-			"ignoreTemplateLiterals": true,
-			"ignorePattern": "\\s*<" //jsx hack https://twitter.com/timtyrrell/status/689912501165658112
-		}],
-        "import/no-extraneous-dependencies": [
-            "error",
-            { "devDependencies": true, "optionalDependencies": true, "peerDependencies": true }
-        ],
-        'import/extensions': ['error', 'always', {
-            js: 'never',
-            jsx: 'never',
-            ts: 'never',
-            tsx: 'never'
-        }]
-	},
-    settings: {
-        'import/resolver': {
-            node: {
-                extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
-            }
-        },
-        react: {
-            pragma: 'React',
-            version: '16.0'
-        }
+  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaVersion": 2017,
+    "ecmaFeatures": {
+      "jsx": true
     }
+  },
+  "plugins": [
+    "react",
+    "prettier"
+  ],
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "rules": {
+    "prettier/prettier": "error"
+  }
 };
