2464819b1f0090359da5570bcf2170a3eb3fadbd
Aubrey Hewes <AubreyHewes@users.noreply.github.com>
2019-10-03 20:17:41 +0200

    extend react-app configuration as new standard (#5)
    * added some extra rules to ensure standard code
    
    fixes 2
    
    * use react-app as upstream standard
     - also includes typescript linting
     - with some added strictness

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 module.exports = {
-  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaVersion": 2017,
-    "ecmaFeatures": {
-      "jsx": true
-    }
-  },
+  "extends": ["react-app", "plugin:prettier/recommended"],
   "plugins": [
-    "react",
     "prettier"
   ],
-  "env": {
-    "es6": true,
-    "node": true
-  },
   "rules": {
-    "prettier/prettier": "error"
+    "prettier/prettier": "error",
+    "no-var": "error",
+    "prefer-const": "error",
+    "prefer-destructuring": "error",
+    "prefer-spread": "error",
+    "prefer-template": "error",
+    "prefer-arrow-callback": "error",
+    "arrow-body-style": "error",
+    "prefer-promise-reject-errors": "error",
+    "prefer-rest-params": "error"
   }
 };
